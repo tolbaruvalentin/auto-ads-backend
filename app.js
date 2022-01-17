939caa8b81d7ad50a00ff0c9dd274f20f0dd393b
Tolbaru Valentin <tolbaruvalentin@gmail.com>
2022-01-18 00:48:17 +0200

    Adaugare express si import in server.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+export default app;
---
+++ server.js
@@ -0,0 +1,8 @@
+import app from './app.js';
+
+
+
+
+
+app.listen(8000, () => console.log(`Serverul a pornit la portul 8000`));
+
